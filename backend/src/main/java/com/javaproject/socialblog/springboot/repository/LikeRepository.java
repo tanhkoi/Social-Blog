930afa201734971925f90a03df131c5193d70bd9
Nguyen Tan Khoi <tankhoi46@gmail.com>
2024-10-30 14:41:20 +0700

    refactor: remove like model

--- backend/src/main/java/com/javaproject/socialblog/springboot/repository/LikeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.javaproject.socialblog.springboot.repository;
-
-import com.javaproject.socialblog.springboot.model.Like;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface LikeRepository extends MongoRepository<Like, String> {
-}
