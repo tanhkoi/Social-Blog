174a9a84825cc32016fb2283e8391d5633832742
Nguyen Tan Khoi <tankhoi46@gmail.com>
2024-11-05 08:49:41 +0700

    feat: limit image size to 5 MB

--- backend/src/main/java/com/javaproject/socialblog/springboot/controller/UploadImageController.java
+++ backend/src/main/java/com/javaproject/socialblog/springboot/controller/UploadImageController.java
@@ -3,6 +3,7 @@ package com.javaproject.socialblog.springboot.controller;
 import com.javaproject.socialblog.springboot.security.service.CloudinaryService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,8 +22,15 @@ public class UploadImageController {
     @Operation(tags = "Upload Service")
     public ResponseEntity<String> uploadImage(@RequestParam("image") MultipartFile file) {
 
-        String data = cloudinaryService.uploadImage(file);
-        return ResponseEntity.ok(data);
+        final long MAX_FILE_SIZE = 5 * 1024 * 1024; // 5 MB
+
+        if (file.getSize() > MAX_FILE_SIZE) {
+            return ResponseEntity.status(HttpStatus.PAYLOAD_TOO_LARGE)
+                    .body("File size exceeds the maximum limit of 2 MB.");
+        }
+
+        String imageUrl = cloudinaryService.uploadImage(file);
+        return ResponseEntity.ok(imageUrl);
     }
 
 }
