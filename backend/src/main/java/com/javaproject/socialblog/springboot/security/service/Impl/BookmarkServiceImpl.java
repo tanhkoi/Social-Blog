9d207404b762aebf546049d25ad8af2547bad69e
Nguyen Tan Khoi <tankhoi46@gmail.com>
2024-11-14 11:25:17 +0700

    fix: remove user field in bookmark ep

--- backend/src/main/java/com/javaproject/socialblog/springboot/security/service/Impl/BookmarkServiceImpl.java
+++ backend/src/main/java/com/javaproject/socialblog/springboot/security/service/Impl/BookmarkServiceImpl.java
@@ -1,11 +1,18 @@
 package com.javaproject.socialblog.springboot.security.service.Impl;
 
 import com.javaproject.socialblog.springboot.model.Bookmark;
+import com.javaproject.socialblog.springboot.model.Post;
+import com.javaproject.socialblog.springboot.model.User;
 import com.javaproject.socialblog.springboot.repository.BookmarkRepository;
+import com.javaproject.socialblog.springboot.repository.PostRepository;
 import com.javaproject.socialblog.springboot.security.service.BookmarkService;
+import com.javaproject.socialblog.springboot.security.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -14,27 +21,54 @@ public class BookmarkServiceImpl implements BookmarkService {
 
     private final BookmarkRepository bookmarkRepository;
 
+    private final UserService userService;
+
+    private final PostRepository postRepository;
+
     // Bookmark a post
-    public void bookmarkPost(String userId, String postId) {
+    public void bookmarkPost(String postId) {
 
-        if (!bookmarkRepository.existsByUserIdAndPostId(userId, postId)) {
+        Authentication loggedInUser = SecurityContextHolder.getContext().getAuthentication();
+        String username = loggedInUser.getName();
 
-            bookmarkRepository.save(new Bookmark(null, userId, postId, null));
+        User currUser = userService.findByUsername(username);
+
+        if (!bookmarkRepository.existsByUserIdAndPostId(currUser.getId(), postId)) {
+
+            bookmarkRepository.save(new Bookmark(null, currUser.getId(), postId, null));
         }
 
     }
 
     // Remove a bookmark
-    public void removeBookmark(String userId, String postId) {
+    public void removeBookmark(String postId) {
 
-        bookmarkRepository.deleteByUserIdAndPostId(userId, postId);
+        Authentication loggedInUser = SecurityContextHolder.getContext().getAuthentication();
+        String username = loggedInUser.getName();
+
+        User currUser = userService.findByUsername(username);
+
+        bookmarkRepository.deleteByUserIdAndPostId(currUser.getId(), postId);
 
     }
 
     // Get all bookmarks for a user
-    public List<Bookmark> getUserBookmarks(String userId) {
+    public List<Post> getUserBookmarks() {
 
-        return bookmarkRepository.findByUserId(userId);
+        Authentication loggedInUser = SecurityContextHolder.getContext().getAuthentication();
+        String username = loggedInUser.getName();
+
+        User currUser = userService.findByUsername(username);
+
+        List<Bookmark> bookmarks = bookmarkRepository.findByUserId(currUser.getId());
+
+        List<Post> posts = new ArrayList<>();
+
+        for (var bookmark : bookmarks) {
+            postRepository.findById(bookmark.getPostId()).ifPresent(posts::add);
+        }
+
+        return posts;
 
     }
 
