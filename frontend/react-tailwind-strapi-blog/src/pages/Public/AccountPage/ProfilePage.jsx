507585580dd215d1c7f09ea8ab4e61be33f6e6d1
NTT203 <anhtienpro203@gmail.com>
2024-12-08 12:37:25 +0700

    fix loading

--- frontend/react-tailwind-strapi-blog/src/pages/Public/AccountPage/ProfilePage.jsx
+++ frontend/react-tailwind-strapi-blog/src/pages/Public/AccountPage/ProfilePage.jsx
@@ -122,13 +122,13 @@ const ProfilePage = () => {
     }
   };
 
-  if (loading) {
-    return (
-      <div className="flex justify-center items-center h-screen bg-white">
-        <p className="text-black text-xl">Loading...</p>
-      </div>
-    );
-  }
+  // if (loading) {
+  //   return (
+  //     <div className="flex justify-center items-center h-screen bg-white">
+  //       <p className="text-black text-xl">Loading...</p>
+  //     </div>
+  //   );
+  // }
 
   return (
     <div className="bg-white min-h-screen text-black">
@@ -143,11 +143,36 @@ const ProfilePage = () => {
           <div className="flex flex-col lg:flex-row gap-4">
             <div className="lg:w-3/4 p-4">
               <h2 className="text-3xl font-bold mb-4 mt-20">Posts</h2>
-              {myPosts.length > 0 ? (
-                <BlogList blogs={myPosts} setBlogs={setMyPosts} />
-              ) : (
-                <p className="text-gray-400">No posts yet.</p>
-              )}
+
+               {loading ? (
+            <div className="flex justify-center items-center mt-10">
+              <svg
+                className="animate-spin h-8 w-8 text-blue-600"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <circle
+                  className="opacity-25"
+                  cx="12"
+                  cy="12"
+                  r="10"
+                  stroke="currentColor"
+                  strokeWidth="4"
+                ></circle>
+                <path
+                  className="opacity-75"
+                  fill="currentColor"
+                  d="M4 12a8 8 0 018-8v8H4z"
+                ></path>
+              </svg>
+              <p className="ml-3 text-gray-400 text-lg">Loading popular posts...</p>
+            </div>
+          ) : myPosts.length > 0  ? (
+            <BlogList blogs={myPosts} setBlogs={setMyPosts} />
+          ) : (
+             <p className="text-gray-400">No posts yet.</p>
+          )}
             </div>
             <div className="lg:w-1/4 lg:h-1/4 bg-card p-4 rounded-lg border border-gray-600">
               <h1 className="text-3xl font-bold mb-5 mt-20 text-center">
