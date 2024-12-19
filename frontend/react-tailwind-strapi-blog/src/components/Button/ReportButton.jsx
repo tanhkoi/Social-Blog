a032118e3929e01198b8f6aa292ed15492daa0dd
viettrung2512 <117735066+viettrung2512@users.noreply.github.com>
2024-12-19 12:30:47 +0700

    feat: UI BlogAudio

--- frontend/react-tailwind-strapi-blog/src/components/Button/ReportButton.jsx
+++ frontend/react-tailwind-strapi-blog/src/components/Button/ReportButton.jsx
@@ -1,37 +1,47 @@
 import React, { useState } from "react";
 
 const ReportButton = ({ reportText, type, id }) => {
-    const [reportStatus, setReportStatus] = useState(null);
+  const [reportStatus, setReportStatus] = useState(null);
 
-    const reportContent = async () => {
-        try {
-            const token = localStorage.getItem('token');
-            const response = await fetch("http://localhost:8080/api/perspective/analyze", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                    "Authorization": `Bearer ${token}`
-                },
-                body: JSON.stringify({ text: reportText, id: id, type: type }),
-            });
-
-            if (response.ok) {
-                setReportStatus("Report submitted successfully!");
-            } else {
-                setReportStatus("Failed to submit report.");
-            }
-        } catch (error) {
-            console.error("Error:", error);
-            setReportStatus("An error occurred while submitting the report.");
+  const reportContent = async () => {
+    try {
+      const token = localStorage.getItem("token");
+      const response = await fetch(
+        "http://localhost:8080/api/perspective/analyze",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+          body: JSON.stringify({ text: reportText, id: id, type: type }),
         }
-    };
+      );
 
-    return (
-        <div>
-            <button onClick={reportContent}>Report</button>
-            {reportStatus && <p>{reportStatus}</p>}
-        </div>
-    );
+      if (response.ok) {
+        setReportStatus("Report submitted successfully!");
+      } else {
+        setReportStatus("Failed to submit report.");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      setReportStatus("An error occurred while submitting the report.");
+    }
+  };
+
+  return (
+    <div className="bg-white text-black">
+      <button
+        onClick={reportContent}
+        className="bg-white text-black px-4 py-2 border border-white rounded hover:text-blue"
+      >
+        Report
+      </button>
+      {reportStatus && (
+        <p className="mt-2 text-sm font-medium">{reportStatus}</p>
+      )}
+    </div>
+  );
 };
 
 export default ReportButton;
